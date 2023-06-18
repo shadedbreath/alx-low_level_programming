a2b3f308a712ddbc216821dd5c7b2551225f6eb8
shadedbreath <mainatitus882@gmail.com>
2023-06-18 10:11:30 -0700

    For loop

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: 'Print from zero to nine'
+ * Return: 0 if successful
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 0; n <= 9; n++)
+	{
+		printf("%d", n);
+	}
+	printf("\n");
+
+		return (0);
+}
