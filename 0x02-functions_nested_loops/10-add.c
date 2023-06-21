210050959e3feb96bb95159eb9eeeb29201f6d69
shadedbreath <mainatitus882@gmail.com>
2023-06-20 17:07:56 -0700

    add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * add - sums up two variables
+ * @n1: the first integer
+ * @n2: the second number
+ * Return: integer sum value
+ */
+int add(int n1, int n2)
+{
+	return (n1 + n2);
+}
