bfd55f31530283c6da7f5d11123e67d534f2e918
shadedbreath <mainatitus882@gmail.com>
2023-06-23 00:37:46 -0700

    print_most_numbers fn

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * print_most_numbers - prints most numbers
+ * Description: prints numbers from 0 to 9 except 2 and 4
+ * Return: numbers from 0 to 9
+ */
+void print_most_numbers(void)
+{
+	int x;
+
+	for (x = 0; x <= 9; x++)
+	{
+		if (x == 2 || x == 4)
+		{
+			continue;
+		}
+		else
+		{
+			_putchar(x + '0');
+		}
+	}
+	_putchar('\n');
+}
