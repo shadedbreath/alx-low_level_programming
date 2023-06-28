63ac073e024420a8f340cb3ebcf0b688010036ea
shadedbreath <mainatitus882@gmail.com>
2023-06-27 19:17:46 -0700

    _strlen

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * _strlen - a function that returns the length of a string
+ * @str: the string to get the length from
+ *
+ * Return: length of @str
+*/
+size_t _strlen(const char *str)
+{
+	size_t length = 0;
+
+	while (*str++)
+		length++;
+	return (length);
+}
