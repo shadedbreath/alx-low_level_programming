7cb5494ee597720fda812b3a0a112beb4d15dfdc
shadedbreath <mainatitus882@gmail.com>
2023-07-05 18:18:54 -0700

    She locked away a secret

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _puts_recursion - a function that prints a string, followed by a new line
+ * @s: input
+ * Return: 0 if successful
+ */
+void _puts_recursion(char *s)
+{
+	if (*s)
+	{
+		_putchar(*s);
+		_puts_recursion(s + 1);
+	}
+	else
+		_putchar('\n');
+}
